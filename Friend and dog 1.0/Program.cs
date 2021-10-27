ba84c63331bbf15e2cbb3fc7f4eb61b6f02e73d6
alexhvorostov <hvorostov.alex1@gmail.com>
2021-10-27 23:01:43 +0400

    Add version 1.0

---
+++ Friend and dog 1.0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+int firstFriendSpeed = 2;
+int secondFriendSpeed = 3;
+int dogSpeed = 5;
+int distance = 10000;
+int friend = 2;
+int time = 0;
+int count = 0;
+
+while (distance > 5)
+{
+    if (friend = 1)
+    {
+        GetTime(time) = GetDistance(distance) / secondFriendSpeed + dogSpeed;
+        friend = 1;
+    }
+    else
+    {
+        GetTime(time) = GetDistance(distance) / firstFriendSpeed + dogSpeed;
+        friend = 2;
+    }
+}
+count++;
+int distance = distance - firstFriendSpeed + secondFriendSpeed * time;
+
+Console.WriteLine(count);
