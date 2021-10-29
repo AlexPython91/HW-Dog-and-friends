6bb97d74fd31e908db8a54603af98a7ee88e3975
alexhvorostov <hvorostov.alex1@gmail.com>
2021-10-29 23:45:01 +0400

    Ввел условия с запросом от пользователя. Плохо

---
+++ Friend and dog 2.0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Console.WriteLine("Приветствуем Вас!");
+
+Console.Write("Введите скорость первого друга: ");
+    string userAnswer = Console.ReadLine();
+    int friend1 = Convert.ToInt32(userAnswer);
+    int firstFriendSpeed = friend1;
+
+Console.Write("Введите скорость второго друга: ");
+    string userAnswer2 = Console.ReadLine();
+    int friend2 = Convert.ToInt32(userAnswer2);
+    int secondFriendSpeed = friend2;
