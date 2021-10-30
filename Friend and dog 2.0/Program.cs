667ff5b022177882e9cb238b57b017b64448b03b
Maria Lipina <m.o.lip714@gmail.com>
2021-10-30 23:19:13 +0300

    без лишних переменных, writeline, из двух строк сделана одна

--- Friend and dog 2.0/Program.cs
+++ Friend and dog 2.0/Program.cs
@@ -1,38 +1,17 @@
-﻿using System;
-
-
-
-Console.WriteLine("Приветствуем Вас!");
+﻿Console.WriteLine("Приветствуем Вас!");
     
     Console.Write("Введите скорость первого друга: ");
-        string userAnswer = Console.ReadLine();
-        int friend1 = Convert.ToInt32(userAnswer);
-        int firstFriendSpeed = friend1;
-        Console.WriteLine("Скорость первого друга = " + friend1);
-
+        int firstFriendSpeed = Convert.ToInt32(Console.ReadLine());
+                        
     Console.Write("Введите скорость второго друга: ");
-        string userAnswer2 = Console.ReadLine();
-        int friend2 = Convert.ToInt32(userAnswer2);
-        int secondFriendSpeed = friend2;
-        Console.WriteLine("Скорость второго друга = " + friend2);
-
+        int secondFriendSpeed = Convert.ToInt32(Console.ReadLine());
+        
     Console.Write("Введите скорость собаки: ");
-        string userAnswer3 = Console.ReadLine();
-        int dog = Convert.ToInt32(userAnswer3);
-        int dogSpeed = dog;
-        Console.WriteLine("Скорость собаки = " + dog);
-
+        int dogSpeed = Convert.ToInt32(Console.ReadLine());
+        
     Console.Write("Введите расстояние: ");
-        string userAnswer4 = Console.ReadLine();
-        int totalDistance = Convert.ToInt32(userAnswer4);
-        int distance = totalDistance;
-        Console.WriteLine("Расстояние = " + totalDistance);
-    
-
-        int FriendSpeedOne = friend1;
-        int FriendSpeedTwo = friend2;
-        int SpeedOfDog = dog;
-        int way = totalDistance; 
+        int distance = Convert.ToInt32(Console.ReadLine());
+          
         int friend = 2;
         int time = 0;
         int count = 0;
